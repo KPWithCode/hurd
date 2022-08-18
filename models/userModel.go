9d3c8dbad3880c131550f74e40070c641d8bc3bb
Kyle Phillips <kphillips375@yahoo.com>
2022-08-18 03:55:27 -0400

    User router

---
+++ models/userModel.go
@@ -0,0 +1,11 @@
+package models
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	ID			int			`json:"id" gorm:"primary_key"`		
+	Username	string		`json:"username" `
+	Email		string		`json:"email"`
+	Password	string		`json:"password"`
+}
