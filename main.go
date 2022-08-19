d4f04f75f9b5e47f7902e292a9c07bdca4404848
Kyle Phillips <kphillips375@yahoo.com>
2022-08-19 00:59:06 -0400

    main setup

--- main.go
+++ main.go
@@ -1,6 +1,17 @@
 package main
-import "fmt"
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/KPWithCode/hurd/config"
+	"github.com/KPWithCode/hurd/routes"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	// use gin.Default() for router with default middleware
+	router := gin.New()
+	router.SetTrustedProxies(nil)
+	config.Connect()
+	
+	routes.UserRoute(router)
+	router.Run(":8080")
 }
