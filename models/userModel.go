9f4f328d148c39b2b673c38f56377a77b301da30
Kyle Phillips <kphillips375@yahoo.com>
2022-10-07 19:51:41 -0400

    gorm addition

--- models/userModel.go
+++ models/userModel.go
@@ -3,8 +3,9 @@ package models
 import "gorm.io/gorm"
 
 type User struct {
+	// In Gorm we need to have Model Fields Capitalised
 	gorm.Model
-	ID			int			`json:"id" gorm:"primary_key"`		
+	ID			int			`json:"ID" gorm:"primary_key"`		
 	Username	string		`json:"username" `
 	Email		string		`json:"email"`
 	Password	string		`json:"password"`
